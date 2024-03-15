17c049ebb19999d56ca25e505bcfaee413578ea7
482F <482F.git.mail@482f.net>
2024-03-16 00:00:00 +0900

    drift

--- main.dart
+++ main.dart
@@ -1,14 +1,31 @@
-import 'dart:ffi';
 import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:sqlite3/sqlite3.dart';
+import 'dart:ffi';
 
 import 'package:path/path.dart';
 import 'package:sqlite3/open.dart';
 
-import './sqlite3.dart' as sqlite3;
+import './lib/database.dart' as db;
 
-void main() {
+void main() async {
   open.overrideFor(OperatingSystem.windows, _openOnWindows);
-  sqlite3.dos();
+  print(db.TodoItems);
+
+  final database = db.AppDatabase(LazyDatabase(() async {
+    return NativeDatabase.opened(sqlite3.openInMemory());
+  }));
+
+  await database.into(database.todoItems).insert(db.TodoItemsCompanion.insert(
+        title: 'todo: finish drift setup',
+        content: 'We can now write queries and define our own tables.',
+      ));
+  List<db.TodoItem> allItems = await database.select(database.todoItems).get();
+
+  print('items in database: $allItems');
+  print(allItems.first.title);
 }
 
 DynamicLibrary _openOnWindows() {
