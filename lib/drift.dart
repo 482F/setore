6ef3c1fdedaf254d154a68fddbb9f7a499d81415
482F <482F.git.mail@482f.net>
2024-03-31 00:00:00 +0900

    impl

--- lib/drift.dart
+++ lib/drift.dart
@@ -1,3 +1,6 @@
+import 'dart:async' as async;
+import 'package:archive/archive.dart' as archive;
+
 import 'package:drift/drift.dart';
 import 'package:setore/sqlite3.dart' as sqlite3;
 
@@ -9,29 +12,78 @@ class Entries extends Table {
   TextColumn get name => text()();
 }
 
-enum FieldType {
-  text,
-  memo,
-  number,
-  date,
-  totpToken,
+enum FieldType<T extends Object> {
+  text<String>(encode: encodeString, decode: decodeString),
+  memo<String>(encode: encodeString, decode: decodeString),
+  date<DateTime>(encode: encodeDate, decode: decodeDate),
+  files<Iterable<archive.ArchiveFile>>(
+      encode: encodeFiles, decode: decodeFiles),
+  totpToken<String>(encode: encodeString, decode: decodeString);
+
+  const FieldType({
+    required this.encode,
+    required this.decode,
+  });
+
+  final async.FutureOr<Uint8List> Function(T?) encode;
+  final async.FutureOr<T?> Function(Uint8List) decode;
+
+  static Uint8List encodeString(String? s) =>
+      Uint8List.fromList((s ?? '').codeUnits);
+  static String decodeString(Uint8List u) => String.fromCharCodes(u);
+  static Uint8List encodeDate(DateTime? d) =>
+      encodeString(d?.toUtc().toIso8601String() ?? '');
+  static DateTime? decodeDate(Uint8List u) {
+    final dateString = decodeString(u);
+    if (dateString.isEmpty) {
+      return null;
+    }
+    return DateTime.parse(dateString).toLocal();
+  }
+
+  static Future<Uint8List> encodeFiles(
+      Iterable<archive.ArchiveFile>? fs) async {
+    final a = archive.Archive();
+    await (fs ?? []).map((f) async {
+      a.addFile(f);
+    }).wait;
+    return Uint8List.fromList(archive.TarEncoder().encode(a));
+  }
+
+  static List<archive.ArchiveFile> decodeFiles(Uint8List u) {
+    return archive.TarDecoder().decodeBytes(u).files;
+  }
 }
 
 class Fields extends Table {
   IntColumn get id => integer().autoIncrement()();
   TextColumn get name => text().unique()();
   BoolColumn get isSecret => boolean()();
-  IntColumn get type => intEnum<FieldType>()();
+  IntColumn get type => intEnum<FieldType<Object>>()();
 }
 
 @DataClassName('EntryField')
 class EntriesFields extends Table {
-  IntColumn get entryId => integer().references(Entries, #id)();
-  IntColumn get fieldId => integer().references(Fields, #id)();
+  IntColumn get id => integer().autoIncrement()();
+  IntColumn get entryId =>
+      integer().references(Entries, #id, onDelete: KeyAction.cascade)();
+  IntColumn get fieldId =>
+      integer().references(Fields, #id, onDelete: KeyAction.restrict)();
   DateTimeColumn get updatedAt => dateTime()();
+  IntColumn get prevField => integer()
+      .nullable()
+      .references(EntriesFields, #id, onDelete: KeyAction.restrict)();
+  IntColumn get nextField => integer()
+      .nullable()
+      .references(EntriesFields, #id, onDelete: KeyAction.restrict)();
+  BlobColumn get value => blob()();
 }
 
-@DriftDatabase(tables: [Entries, Fields, EntriesFields])
+@DriftDatabase(tables: [
+  Entries,
+  Fields,
+  EntriesFields,
+])
 class AppDatabase extends _$AppDatabase {
   AppDatabase(
     final String path, {
--- lib/setore.dart
+++ lib/setore.dart
@@ -1,4 +1,10 @@
+import 'dart:typed_data' show Uint8List;
+
+import 'package:drift/drift.dart' show innerJoin;
 import 'package:setore/drift.dart' as drift;
+import 'package:setore/result.dart' show Result;
+
+typedef InsertedGetter<T extends Object?> = ({Future<T> Function() get});
 
 class Setore {
   final drift.AppDatabase appDb;
@@ -12,17 +18,108 @@ class Setore {
           passphrase: passphrase,
         );
 
-  void createFields(
-      final List<({String name, bool isSecret, drift.FieldType type})>
-          fields) async {
+  Future<Result<InsertedGetter<List<drift.Entry>>, List<String>>> createEntries(
+    final Iterable<({String name})> entriesForInsert,
+  ) async {}
+
+  Future<List<drift.Entry>> readEntriesByNames(
+    final Iterable<String> names,
+  ) async {}
+
+  Future<drift.Entry?> readEntryByName(
+    final String name,
+  ) async {}
+
+  Future<Result<Null, List<drift.Field>>> updateEntries(
+    final Iterable<drift.Entry> entries,
+  ) async {}
+
+  Future<Result<Null, List<int>>> deleteEntries(
+    final Iterable<int> ids,
+  ) async {}
+
+  Future<Result<InsertedGetter<List<drift.EntryField>>, List<drift.Entry>>>
+      createEntriesFields(
+    final drift.Entry entry,
+    final Iterable<({drift.Field field, Uint8List value})>
+        entriesFieldsForInsert,
+  ) async {}
+
+  Future<List<drift.EntryField>> readEntriesFieldsByEntry(
+    final drift.Entry entry,
+  ) async {
+    // 順番通りにソート
+  }
+
+  Future<Result<Null, List<drift.EntryField>>> updateEntriesFields(
+    final Iterable<drift.EntryField> entriesFields,
+  ) async {}
+
+  Future<Result<Null, List<int>>> deleteEntriesFields(
+    final Iterable<int> ids,
+  ) async {}
+
+  Future<Result<InsertedGetter<List<drift.Field>>, List<String>>> createFields(
+    final Iterable<({String name, bool isSecret, drift.FieldType type})>
+        fieldsForInsert,
+  ) async {
+    // TODO: return created fields
+    // TODO: name unique validation
     await appDb.batch((batch) {
       batch.insertAll(
-          appDb.fields,
-          fields.map((field) => drift.FieldsCompanion.insert(
-                name: field.name,
-                isSecret: field.isSecret,
-                type: field.type,
-              )));
+        appDb.fields,
+        fieldsForInsert.map(
+          (field) => drift.FieldsCompanion.insert(
+            name: field.name,
+            isSecret: field.isSecret,
+            type: field.type,
+          ),
+        ),
+      );
     });
+  }
+
+  Future<List<drift.Field>> readFieldsByNames(
+    final Iterable<String> names,
+  ) async {
+    return await (appDb.select(appDb.fields)
+          ..where((fields) => fields.name.isIn(names)))
+        .get();
+  }
+
+  Future<drift.Field?> readFieldByName(
+    final String name,
+  ) async {
+    return await (appDb.select(appDb.fields)
+          ..where((fields) => fields.name.equals(name)))
+        .getSingleOrNull();
+  }
+
+  Future<Result<Null, List<drift.Field>>> updateFields(
+    final Iterable<drift.Field> fields,
+  ) async {
+    // TODO: validation
+    final usingEntries = await (appDb.select(appDb.entries).join([
+      innerJoin(
+        appDb.entriesFields,
+        appDb.entries.id.equalsExp(appDb.entriesFields.entryId),
+      ),
+    ])
+          ..where(
+            appDb.entriesFields.fieldId.isIn(fields.map((field) => field.id)),
+          ))
+        .get();
+
+    await appDb.batch((batch) {
+      batch.replaceAll(appDb.fields, fields);
+    });
+  }
+
+  Future<Result<Null, List<int>>> deleteFields(
+    final Iterable<int> ids,
+  ) async {
+    // TODO: validation
+    await (appDb.delete(appDb.fields)..where((fields) => fields.id.isIn(ids)))
+        .go();
   }
 }
