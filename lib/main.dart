18ca2a46523f393b945b0af5f7404c24751e7556
482F <482F.git.mail@482f.net>
2024-04-20 00:00:00 +0900

    verify page

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:setore/setore.dart' show Setore;
+import 'package:setore/verify.dart';
 
 part 'main.g.dart';
 
 void main() async {
-  runApp(const ProviderScope(child: App()));
-  final setore = Setore(
-    './s.sq3',
-    dllPathForWindows: './sqlcipher.dll',
-    passphrase: 'asdf',
-  );
-  await setore.createEntries([(name: 'cr test${DateTime.now().toString()}')]);
-  print(await setore.readEntries());
+  runApp(ProviderScope(child: App()));
+  // final setore = Setore(
+  //   './s.sq3',
+  //   dllPathForWindows: './sqlcipher.dll',
+  //   passphrase: 'asdf',
+  // );
+  // await setore.createEntries([(name: 'cr test${DateTime.now().toString()}')]);
+  // print(await setore.readEntries());
+}
+
+@riverpod
+class _Setore extends _$Setore {
+  @override
+  Setore? build() {
+    return null;
+  }
+
+  void update(Setore? setore) {
+    state = setore;
+  }
 }
 
 class App extends StatelessWidget {
@@ -30,13 +43,30 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Flex(
-          direction: Axis.horizontal,
-          children: [
-            V(Adder()),
-            V(Adder()),
-            // V(_Adder()),
-          ],
+        body: Consumer(
+          builder: (context, ref, _) {
+            final setore = ref.watch(_setoreProvider);
+            if (setore == null) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Verify(
+                    onVerified: (setore) =>
+                        ref.read(_setoreProvider.notifier).update(setore),
+                  ),
+                ),
+              );
+            } else {
+              setore.readEntries().then((es) => print(es));
+              return FutureBuilder(
+                  future: setore.readEntries().then((es) => es.first.name),
+                  builder: (context, snapshot) => switch (snapshot) {
+                        AsyncSnapshot(hasData: true, data: final data) =>
+                          Text(data ?? 'null'),
+                        _ => Text('yet'),
+                      });
+            }
+          },
         ),
       ),
     );
