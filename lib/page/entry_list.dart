91644ab1ac5bfcfb1062245801ea9e2d3170acea
482F <482F.git.mail@482f.net>
2024-04-30 00:00:00 +0900

    listview

--- lib/page/entry_list.dart
+++ lib/page/entry_list.dart
@@ -1,55 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:setore/app_state.dart';
+import 'package:setore/component/list-view-with-sticky-header.dart';
 
 final List<String> l = [];
 
-class ListViewWithStickyHeader<I> extends StatelessWidget {
-  ListViewWithStickyHeader({
-    super.key,
-    required this.header,
-    required this.items,
-    required this.itemBuilder,
-  });
-
-  final Widget header;
-  final List<I>? items;
-  final Widget Function(I, int) itemBuilder;
-
-  @override
-  build(BuildContext context) {
-    return NestedScrollView(
-      headerSliverBuilder: (context, bodyIsScrolled) => [
-        SliverOverlapAbsorber(
-          handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-          sliver: SliverAppBar(
-            automaticallyImplyLeading: false,
-            title: header,
-            pinned: true,
-          ),
-        )
-      ],
-      body: Builder(
-        builder: (context) => CustomScrollView(
-          slivers: [
-            SliverOverlapInjector(
-              // This is the flip side of the SliverOverlapAbsorber
-              // above.
-              handle:
-                  NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-            ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (context, i) => itemBuilder(items![i], i),
-                childCount: items?.length ?? 0,
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
 
 class EntryList extends ConsumerWidget {
   const EntryList({super.key});
