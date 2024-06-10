7abaa2c3d9211639b76cf187c6035df1d002942a
482F <482F.git.mail@482f.net>
2024-06-11 00:00:00 +0900

    temp

--- lib/page/main_page/entry_list.dart
+++ lib/page/main_page/entry_list.dart
@@ -1,23 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:setore/app_state.dart';
 import 'package:setore/component/list_view_with_sticky_header.dart';
 import 'package:setore/drift.dart';
 
-final List<String> l = [];
-
 class EntryList extends ConsumerWidget {
-  const EntryList({super.key, required this.onOpenItem});
+  const EntryList({super.key, required this.onOpenItem, required this.entries});
 
   final void Function(Entry) onOpenItem;
+  final List<Entry>? entries;
 
   @override
   build(BuildContext context, WidgetRef ref) {
-    final entries = switch (ref.watch(entriesProvider(l))) {
-      AsyncError(error: final e) => throw e,
-      AsyncData(value: final entries) => entries,
-      _ => null,
-    };
     return ListViewWithStickyHeader(
       header: const Text('header'),
       items: entries,
